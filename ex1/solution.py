7b76b44bdbae7fdecd16912439b14ad8b3c395d8
nikit <nikire@ttu.ee>
2022-11-22 00:08:43 +0200

    wisercat

--- ex1/solution.py
+++ ex1/solution.py
@@ -1,20 +1,22 @@
-"""Solution."""
+"""WiserCat."""
 
 
 def reversed_array_into_the_text_file(filename: str):
     """Read file contents into string."""
     mylist = []  # create list
-    with open(filename, 'r') as file:  # reading the file
+    with open(filename, 'r', encoding='utf-8') as file:  # reading the file
         for line in file:
-            for elements in line:
-                if elements.isdigit():  # digital check
-                    mylist.append(str(elements))  # add to the list
-                else:
-                    continue
+            if len(line) > 2:
+                divide = line[1:-1].split(",")  # divide by a comma
+                for elements in divide:
+                    integer = int(elements)
+                    mylist.append(integer)  # add to the list
+            else:
+                print("Empty!!!")
     print(mylist)
     if len(mylist) > 0:
         print(mylist[::-1])  # reverse list
-        with open(filename, 'w') as file:
+        with open(filename, 'w', encoding='utf-8') as file:
             for reversed_elements in str(mylist[::-1]):
                 file.write(reversed_elements)
 
